279432583f14e9d3326fb4d275ddf8e6057950fa
vhvictorhugo <victor.h.santos@ufv.br>
2024-06-21 11:34:55 -0300

    refactor: export region_embedding as parquet file

--- region-embedding/baselines/poi-encoder/region_embedding.py
+++ region-embedding/baselines/poi-encoder/region_embedding.py
@@ -14,7 +14,8 @@ def main(h3=False):
     embeddings = torch.load('./data/poi-encoder.tensor')['in_embed.weight']
     pois = pd.read_csv('./data/pois.csv')[['fclass', column]]
 
-    pois['embedding'] = pois['fclass'].apply(lambda x: np.array(embeddings[x]))
+    # pois['embedding'] = pois['fclass'].apply(lambda x: np.array(embeddings[x]))
+    pois['embedding'] = pois['fclass'].apply(lambda x: embeddings[x].cpu().numpy())
     cta = pois[column].unique().tolist()
 
     d = defaultdict(list)
@@ -24,7 +25,8 @@ def main(h3=False):
     region = pd.DataFrame.from_dict(d, orient='index', columns=[str(i) for i in range(64)])
     region.index.name = name_index
     region = region.reset_index()
-    region.to_csv('./data/region_embedding.csv', index=False)
+    # region.to_csv('./data/region_embedding.csv', index=False)
+    region.to_parquet('./data/region_embedding.parquet', index=False)
     
 if __name__ == "__main__":
     main(h3=True)
